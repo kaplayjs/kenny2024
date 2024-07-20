8af2d8214ac990a7d00fcf04ab1a2b1c54e08abd
Tristan Perrow <tsperrow49@gmail.com>
2024-07-20 15:19:44 -0400

    extended level, added waypoints, added packages

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,3 +1,4 @@
+import { play, Vec2, vec2 } from "kaplay";
 import { k } from "../kaplay";
 import { player } from "../player/player";
 import { $tutorEnabled } from "../stores";
@@ -16,21 +17,26 @@ k.scene("game", () => {
 
     // Adds level to scene
     let level = k.addLevel([
-        "=-=+-=a g-----------",
-        "-+-=qer gc----------",
-        "t-==a   weeeeeet----",
-        "g-c-a          wt---",
-        "weeer           g---",
-        "                g---",
-        "                wt--",
-        "                 gc-",
-        "                 g--",
-        "2x              zb--",
-        "-1x             g---",
-        "--a             g---",
-        "-ca          z223---",
-        "qer       z223------",
-        "a     z2223c--------",
+        "=-=+-=a        g-===-+-=+OoPP",
+        "=-=+-=a        g-===-+-=+OoPP",
+        "=-=+-=a        g-===-+-=+OoPP",
+        "-+P=qer        gc+=p+o====pPo",
+        "tO==a          weeeeeet=+==Op",
+        "g+c=a                 wt==-=o",
+        "weeer                  g-p=-=",
+        "                       g==-=-",
+        "                       wt=o=-",
+        "                        gc==-",
+        "                        g-==-",
+        "2x                     zb-o=-",
+        "-1x                    g-==-+",
+        "+=a                    g=+==-",
+        "=ca                 z223p=+=-",
+        "qer              z223=-==-=-o",
+        "a            z2223c-==o=+=-Op",
+        "a            g--=+=-====-+oPP",
+        "a            g--=+=-====-+oPP",
+        "a            g--=+=-====-+oPP",
     ], {
         tileWidth: 16,
         tileHeight: 16,
@@ -42,9 +48,8 @@ k.scene("game", () => {
             "-": () => [
                 k.pos(),
                 k.sprite("TinyBattle_0_0"),
-                // k.area(),
-                // k.body({ isStatic: true }),
-                "wall",
+                //k.area(),
+                //k.body({ isStatic: true })
             ],
             "=": () => [
                 k.pos(),
@@ -106,36 +111,293 @@ k.scene("game", () => {
                 k.pos(),
                 k.sprite("TinyBattle_2_3"),
             ],
+            "o": () => [
+                k.pos(),
+                k.sprite("TinyBattle_0_0"),
+                {
+                    add() {
+                        this.add([
+                            k.pos(),
+                            k.sprite("TinyBattle_4_5"),
+                        ])
+                    }
+                }
+            ],
+            "O": () => [
+                k.pos(),
+                k.sprite("TinyBattle_1_0"),
+                {
+                    add() {
+                        this.add([
+                            k.pos(),
+                            k.sprite("TinyBattle_4_5"),
+                        ])
+                    }
+                }
+            ],
+            "p": () => [
+                k.pos(),
+                k.sprite("TinyBattle_0_0"),
+                {
+                    add() {
+                        this.add([
+                            k.pos(),
+                            k.sprite("TinyBattle_4_6"),
+                        ])
+                    }
+                }
+            ],
+            "P": () => [
+                k.pos(),
+                k.sprite("TinyBattle_1_0"),
+                {
+                    add() {
+                        this.add([
+                            k.pos(),
+                            k.sprite("TinyBattle_4_6"),
+                        ])
+                    }
+                }
+            ],
             "c": () => [
                 k.pos(),
                 k.sprite("TinyBattle_0_0"),
                 k.area(),
                 k.body({ isStatic: true }),
+                k.offscreen({ hide: true }),
+                "City",
                 {
                     add() {
                         this.add([
                             k.pos(),
-                            k.color(50, 50, 50),
+                            k.color(50,50,50),
                             k.sprite("TinyBattle_0_6"),
-                        ]);
+                        ])
                         this.add([
                             k.pos(0, -4),
                             k.sprite("TinyBattle_10_" + Math.floor(Math.random() * 5)),
-                            "City",
-                        ]);
-                    },
-                },
-            ],
-        },
-    });
+                            k.z(5),
+                        ])
+                    }
+                }
+            ]
+        }
+    })
 
     // A "Game Object" is the basic unit of entity in kaboom
     // Game objects are composed from components
     // Each component gives a game object certain capabilities
 
     // add() assembles a game object from a list of components and add to game, returns the reference of the game object
+    let plr = player(level);
 
-    player(level);
+    let score = 0
+    let selectedPackageIndex = 0
+    let packages = []
+    let packagesUI = [
+        k.add([
+            k.pos(k.width() - 48, k.height() - 16),
+            k.anchor("center"),
+            k.rect(12,12),
+            k.color(255,255,255),
+            k.fixed(),
+            k.scale(1),
+            {
+                add() {
+                    this.add([
+                        k.pos(0, 0),
+                        k.anchor("center"),
+                        k.sprite("TinyBattle_0_6"),
+                        k.color(150, 75, 0),
+                        k.scale(0.5),
+                        k.opacity(1.0),
+                        k.z(5),
+                    ])
+                }
+            },
+        ]),
+        k.add([
+            k.pos(k.width() - 32, k.height() - 16),
+            k.anchor("center"),
+            k.rect(12,12),
+            k.color(255,255,255),
+            k.fixed(),
+            k.scale(1),
+            {
+                add() {
+                    this.add([
+                        k.pos(0, 0),
+                        k.anchor("center"),
+                        k.sprite("TinyBattle_0_6"),
+                        k.color(150, 75, 0),
+                        k.scale(0.5),
+                        k.opacity(1.0),
+                        k.z(5),
+                    ])
+                }
+            },
+        ]),
+        k.add([
+            k.pos(k.width() - 16, k.height() - 16),
+            k.anchor("center"),
+            k.rect(12,12),
+            k.color(255,255,255),
+            k.fixed(),
+            k.scale(1),
+            {
+                add() {
+                    this.add([
+                        k.pos(0, 0),
+                        k.anchor("center"),
+                        k.sprite("TinyBattle_0_6"),
+                        k.color(150, 75, 0),
+                        k.scale(0.5),
+                        k.opacity(1.0),
+                        k.z(5),
+                    ])
+                }
+            },
+        ])
+    ]
+    let waypointUI = k.add([
+        k.pos(),
+        k.sprite("TinyBattle_7_2"),
+        k.rotate(0),
+        k.opacity(1.0),
+        k.scale(1),
+    ])
+
+    // fun names
+    const PACKAGE_ADJECTIVES = [
+        "Cursed",
+        "Dull",
+        "Smelly",
+        "Odd",
+        "Suspicious",
+        "Soggy",
+        "Sticky",
+        "Toy",
+        "Normal",
+        "Bizarre",
+        "Delicate",
+        "Colorful",
+        "Shiny",
+        "Golden",
+        "Pristine",
+        "Magical",
+        "Mythical",
+        "Radiant",
+        "Perfect",
+    ]
+    const PACKAGE_ITEMS = [
+        "Letter",
+        "Necklace",
+        "Robot",
+        "Gemstone",
+        "Treasure Map",
+        "Key",
+        "Painting",
+        "Compass",
+        "Golden Coin",
+        "Box",
+        "Scroll",
+        "Soldier",
+        "Playing Cards",
+        "Telescope",
+        "Bottle",
+        "Instrument",
+        "Shoes",
+        "Fan",
+        "Mug",
+        "Clothes",
+        "Candle",
+        "Drinks",
+        "Food",
+        "Kitchen Supplies",
+        "Computer"
+    ]
+
+    // adjustable for package timing
+    let packageTimerRange = 20
+    let packageTimeBase = 20
+
+    let Cities = level.get("City")
+    function makePackage() {
+        return {
+            name: PACKAGE_ADJECTIVES[Math.floor(Math.random() * PACKAGE_ADJECTIVES.length)] + " " + PACKAGE_ITEMS[Math.floor(Math.random() * PACKAGE_ITEMS.length)],
+            target: Cities[Math.floor(Math.random() * Cities.length)],
+            time: Math.random() * packageTimerRange + packageTimeBase
+        }
+    }
+
+    async function deliverPackage(i) {
+        if (!packages[i]) return;
+        if (packages[i].time <= 0) {
+            // failed to deliver package in time, even though they delivered it. (error)
+        } else {
+            score += packages[i].time * 10;
+            console.log("Delivered the package: (" + packages[i].name + ", " + packages[i].target + ", " + packages[i].time + ")!");
+            packages[i] = null
+        }
+        await new Promise(resolve => setTimeout(resolve, 10 * 1000));   // time to receive new packages? or just could be instant with some delivered feedback
+        packages[i] = makePackage();
+    }
+
+    function playGame() {        
+        for (var i = 0; i < 3; i++) {
+            packages[i] = makePackage()
+        }
+    }
+    function isOnScreen(pos) {
+        const screenRect = new k.Rect(k.vec2(0, 16), k.width(), k.height());
+        return !k.testRectPoint(screenRect, pos)
+                && screenRect.sdistToPoint(pos) > 0 * 0;
+    }
+
+    k.onUpdate(() => {
+        for (var i = 0; i < 3; i++) {
+            packagesUI[i].scale = k.vec2(1, 1);
+
+            // check if player has a package in the slot.
+            if (!packages[i]) {
+                packagesUI[i].children[0].opacity = 0.0
+                continue;
+            }
+            packagesUI[i].children[0].opacity = 1.0
+
+            // timer for delivering package
+            packages[i].time -= k.dt()
+            if (packages[i].time <= 0) {
+                // remove package from packages and make new one in 10 seconds... (TODO)
+                packages[i] = null
+                continue;
+            }
+        }
+        packagesUI[selectedPackageIndex].scale = k.vec2(1.33,1.33);
+
+        // update waypoint
+        let selectedPackage = packages[selectedPackageIndex]
+        if  (selectedPackage) {
+            if (isOnScreen(selectedPackage.target.screenPos())) {
+                waypointUI.opacity = 1.0;
+                waypointUI.angle = k.vec2(selectedPackage.target.pos.add(8,8)).angle(plr.player.pos);
+                waypointUI.pos = plr.player.pos.add(k.Vec2.fromAngle(waypointUI.angle).scale(k.wave(45, 55, k.time())));
+            } else {
+                waypointUI.angle = 90;
+                waypointUI.pos = selectedPackage.target.pos.add(16, k.wave(-16, -20, k.time() * 2));
+            }
+        } else {
+            waypointUI.opacity = 0.0;
+        }
+    })
+
+    k.onKeyPress(["1", "2", "3"], (key) => {
+        selectedPackageIndex = Number(key) - 1
+    })
+
+    k.onKeyPress("f", () => {
+        deliverPackage(selectedPackageIndex);
+    });
 
     const tutorText = k.add([
         k.text("Use\nArrow\nKeys  To  Move", { font: "KennyPixel", size: 16, align: "center" }),
@@ -152,6 +414,7 @@ k.scene("game", () => {
             playingSong.loop = true;
             playingSong.seek(menuSong.time());
             menuSong.stop();
+            playGame();
         }
         $tutorEnabled.set(false);
     });
